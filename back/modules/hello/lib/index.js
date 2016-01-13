27d8a28e041f46894a24da153abe269d66203f17
hiromoon <hiromoon0428@gmail.com>
2016-01-13 20:56:03 +0900

    react for JSX

--- back/modules/hello/lib/index.js
+++ back/modules/hello/lib/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * Lib
  */
@@ -5,13 +7,18 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var testComponent = React.createClass({
-  render: function() {
-            return React.createElement('span', {className: "testComponent"}, "hello");
-          }
+  displayName: 'testComponent',
+
+  render: function render() {
+    return React.createElement(
+      'span',
+      { className: 'testComponent' },
+      '"hello"'
+    );
+  }
 });
 
-module.exports.respond = function(event, cb) {
-  return cb(null, React.renderToStaticMarkup(
-                    React.createElement(testComponent, null)
-          ));
+module.exports.respond = function (event, cb) {
+  return cb(null, React.renderToStaticMarkup(React.createElement('testComponent', null)));
 };
+
---
+++ back/modules/hello/src/index.js
@@ -0,0 +1,15 @@
+/**
+ * Lib
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var testComponent = React.createClass({
+  render: function() {
+            return (<span className="testComponent">"hello"</span>);
+          }
+});
+
+module.exports.respond = function(event, cb) {
+  return cb(null, React.renderToStaticMarkup(<testComponent />));
+};
